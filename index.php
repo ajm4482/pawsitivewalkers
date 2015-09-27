7d7a716506139c89ead8a6734c1ed974baaafc40
Kevin Cavazos <cavazoskevin21@gmail.com>
2015-09-27 01:15:25 -0500

    PHP was closed

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
 <html lang = "en-US">
- <head>
- <meta charset = "UTF-8">
- <title>contact.php</title>
- <style type = "text/css">
-  table, th, td {border: 1px solid black};
- </style>
- </head>
- <body>
- <p>
  <?php
  	function OpenConnection()
 {
@@ -50,6 +41,7 @@ function ReadData()
         echo("Error!");
     }
 }
- </p>
+ReadData();
+?>
  </body>
 </html>
