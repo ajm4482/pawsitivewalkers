00a12f39be357462c5d01995fa5f81196c61bb5b
Kevin Cavazos <cavazoskevin21@gmail.com>
2015-09-27 06:41:56 -0500

    Attempt to push fake dog to pawsitivedog

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     {
         echo "1";
         $serverName = "tcp:pawsitivedogs.database.windows.net,1433";
-        $connectionOptions = array("Database"=>"Pawsitive",
+        $connectionOptions = array("Database"=>"PawsitiveDogs",
             "Uid"=>"andymedina11@pawsitivedogs.database.windows.net", "PWD"=>"HackTX1!");
         $conn = sqlsrv_connect($serverName, $connectionOptions);
         echo "6";
@@ -29,7 +29,7 @@ function WriteData()
     {
         echo "622";
         $conn = OpenConnection();
-        $tsql = "INSERT INTO SalesLT.Customers (CustomerID, NameStyle, Title, FirstName, MiddleName, LastName, Suffix, CompanyName, SalesPerson, EmailAddress, Phone,PasswordHash,PasswordSalt,rowguid,ModifiedDate) VALUES (Rosco Jenkins,S,SOU,NULL,BikeDude,adventure-workspamela,some@email,10294-19293-1,lqocjpqoiqj398qEEQa,13iakwl,aj020maij,2005-08-01 00:00)";
+        $tsql = "INSERT INTO dbo.Dog (ID,Name,Count,Time1,Time2,Time3,Time4,Time5,Time6,TimeDiff,Notes,Kennel,Image) VALUES (Rosco Jenkins,10,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL)";
         if (!sqlsrv_query($conn, $tsql))
         {
             echo "dieee";
